3bf04e4d377b05d0291acb09c6ff0fcf2f5668ef
Tanishka Sharma <tanishkaratlam@gmail.com>
2025-06-27 21:09:32 +0530

    Resume Updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -121,7 +121,7 @@ export default function Hero() {
         </HeroText>
 
         <HeroButton
-          href="Tanishka_Resume_Frontend_FaangPath.pdf"
+          href="Tanishka_Resume_SDE_FaangPath"
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5, delay: 0.8 }}
